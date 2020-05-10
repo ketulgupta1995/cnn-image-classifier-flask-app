1b1e400aaa621c4e3afa523befdc27750d441a2a
ubuntu <justforfun631995@gmail.com>
2020-05-10 13:24:33 +0530

    make it better

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template("index.html",
+                           default_category="Bag", categories=['T-shirt/top',
+                                                            'Trouser',
+                                                            'Pullover',
+                                                            'Dress',
+                                                            'Coat',
+                                                            'Sandal',
+                                                            'Shirt',
+                                                            'Sneaker',
+                                                            'Bag',
+                                                            'Ankle boot'])
+
 
 if __name__ == '__main__':
     app.run()
